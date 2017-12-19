4b3e7d3b9056609526da20195eec2621e7f0f2f7
Denny McFadden <dennymcfadden@gmail.com>
2017-12-19 10:09:05 -0500

    Implemented form fields and scoring logic

--- html/js/score_logic.js
+++ html/js/score_logic.js
@@ -1,300 +1,291 @@
-// When the value in #roll changes
-$('#roll').change(function() {
- // Get the result from the dropdown
- const rollResult = $(this).val();
- // Set the global variable A1?
+$("#submit").click(function() {
+    // Set variable for each ball where A/B/C refers to the frame #, and 1/2 refers to the ball #.
+    const a1 = parseInt($("#a1").val());
+    const a2 = parseInt($("#a2").val());
+    const b1 = parseInt($("#b1").val());
+    const b2 = parseInt($("#b2").val());
+    const c1 = parseInt($("#c1").val());
+    const c2 = parseInt($("#c2").val());
+    const d1 = parseInt($("#d1").val());
+    const d2 = parseInt($("#d2").val());
+    const e1 = parseInt($("#e1").val());
+    const e2 = parseInt($("#e2").val());
+    const f1 = parseInt($("#f1").val());
+    const f2 = parseInt($("#f2").val());
+    const g1 = parseInt($("#g1").val());
+    const g2 = parseInt($("#g2").val());
+    const h1 = parseInt($("#h1").val());
+    const h2 = parseInt($("#h2").val());
+    const i1 = parseInt($("#i1").val());
+    const i2 = parseInt($("#i2").val());
+    const j1 = parseInt($("#j1").val());
+    const j2 = parseInt($("#j2").val());
+    const j3 = parseInt($("#j3").val());
+
+    // Calculate result of frame one. Consider strikes & spares.
+    function calcFrameOne() {
+      if (a1===10 && b1===10 && c1) {
+        return a1+b1+c1;
+      } else if (a1===10 && b1 && b2) {
+        return a1+b1+b2;
+      } else if (a1+a2===10 && b1) {
+        return a1+a2+b1;
+      } else if (a1 && a2 && a1+a2<10) {
+        return a1+a2;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame two. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameTwo() {
+      if (b1===10 && c1===10 && d1) {
+        const score = b1+c1+d1;
+        return score + frameOneScore;
+      } else if (b1===10 && c1 && c2) {
+        const score = b1+c1+c2;
+        return score + frameOneScore;
+      } else if (b1+b2===10 && c1) {
+        const score = b1+b2+c1;
+        return score + frameOneScore;
+      } else if (b1 && b2 && b1+b2<10) {
+        const score = b1+b2;
+        return score + frameOneScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame three. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameThree() {
+      if (c1===10 && d1===10 && e1) {
+        const score = c1+d1+e1;
+        return score + frameTwoScore;
+      } else if (c1===10 && d1 && d2) {
+        const score = c1+d1+d2;
+        return score + frameTwoScore;
+      } else if (c1+c2===10 && d1) {
+        const score = c1+c2+d1;
+        return score + frameTwoScore;
+      } else if (c1 && c2 && c1+c2<10) {
+        const score = c1 + c2;
+        return score + frameTwoScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame four. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameFour() {
+      if (d1===10 && e1===10 && f1) {
+        const score = d1+e1+f1;
+        return score + frameThreeScore;
+      } else if (d1===10 && e1 && e2) {
+        const score = d1+e1+e2;
+        return score + frameThreeScore;
+      } else if (d1+d2===10 && e1) {
+        const score = d1+d2+e1;
+        return score + frameThreeScore;
+      } else if (d1 && d2 && d1+d2<10) {
+        const score = d1+d2;
+        return score + frameThreeScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame five. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameFive() {
+      if (e1===10 && f1===10 && g1) {
+        const score = e1+f1+g1;
+        return score + frameFourScore;
+      } else if (e1===10 && f1 && f2) {
+        const score = e1+f1+f2;
+        return score + frameFourScore;
+      } else if (e1+e2===10 && f1) {
+        const score = e1+e2+f1;
+        return score + frameFourScore;
+      } else if (e1 && e2 && e1+e2<10) {
+        const score = e1+e2;
+        return score + frameFourScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame six. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameSix() {
+      if (f1===10 && g1===10 && h1) {
+        const score = f1+g1+h1;
+        return score + frameFiveScore;
+      } else if (f1===10 && g1 && g2) {
+        const score = f1+g1+g2;
+        return score + frameFiveScore;
+      } else if (f1+f2===10 && g1) {
+        const score = f1+f2+g1;
+        return score + frameFiveScore;
+      } else if (f1 && f2 && f1+f2<10) {
+        const score = f1+f2;
+        return score + frameFiveScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame seven. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameSeven() {
+      if (g1===10 && h1===10 && i1) {
+        const score = g1+h1+i1;
+        return score + frameSixScore;
+      } else if (h1===10 && h1 && h2) {
+        const score = g1+h1+h2;
+        return score + frameSixScore;
+      } else if (g1+g2===10 && h1) {
+        const score = g1+g2+h1;
+        return score + frameSixScore;
+      } else if (g1 && g2 && g1+g2<10) {
+        const score = g1+g2;
+        return score + frameSixScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame eight. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameEight() {
+      if (h1===10 && i1===10 && j1) {
+        const score = h1+i1+j1;
+        return score + frameSevenScore;
+      } else if (h1===10 && i1 && i2) {
+        const score = h1+i1+i2;
+        return score + frameSevenScore;
+      } else if (h1+h2===10 && i1) {
+        const score = h1+h2+i1;
+        return score + frameSevenScore;
+      } else if (h1 && h2 && h1+h2<10) {
+        const score = h1+h2;
+        return score + frameSevenScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame nine. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameNine() {
+      if (i1===10 && j1===10 && j3) {
+        const score = i1+j1+j3;
+        return score + frameEightScore;
+      } else if (i1===10 && j1 && j2) {
+        const score = i1+j1+j2;
+        return score + frameEightScore;
+      } else if (i1+i2===10 && j1) {
+        const score = i1+i2+j1;
+        return score + frameEightScore;
+      } else if (i1 && i2 && i1+i2<10) {
+        const score = i1+i2;
+        return score + frameEightScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Calculate result of frame ten. Consider strikes & spares. Add to sum of previous frames.
+    function calcFrameTen() {
+      if (j1===10 && j2===10 && j3) {
+        const score = j1+j2+j3;
+        return score + frameNineScore;
+      } else if (j1===10 && j2 && j3) {
+        const score = j1+j2+j3;
+        return score + frameNineScore;
+      } else if (j1+j2===10 && j3) {
+        const score = j1+j2+j3;
+        return score + frameNineScore;
+      } else if (j1 && j2 && j1+j2<10) {
+        const score = j1+j2;
+        return score + frameNineScore;
+      } else {
+        return '';
+      }
+    }
+
+    // Store result of calcFrame{X} function in a variable
+    var frameOneScore = calcFrameOne();
+    var frameTwoScore = calcFrameTwo();
+    var frameThreeScore = calcFrameThree();
+    var frameFourScore = calcFrameFour();
+    var frameFiveScore = calcFrameFive();
+    var frameSixScore = calcFrameSix();
+    var frameSevenScore = calcFrameSeven();
+    var frameEightScore = calcFrameEight();
+    var frameNineScore = calcFrameNine();
+    var frameTenScore = calcFrameTen();
+
+    // Calculate current score.
+    function currentScore() {
+      if (frameTenScore) {
+        return frameTenScore;
+      } else if (frameNineScore) {
+        return frameNineScore;
+      } else if (frameEightScore) {
+        return frameEightScore;
+      } else if (frameSevenScore) {
+        return frameSevenScore;
+      } else if (frameSixScore) {
+        return frameSixScore;
+      } else if (frameFiveScore) {
+        return frameFiveScore;
+      } else if (frameFourScore) {
+        return frameFourScore;
+      } else if (frameThreeScore) {
+        return frameThreeScore;
+      } else if (frameTwoScore) {
+        return frameTwoScore;
+      } else if (frameOneScore) {
+        return frameOneScore;
+      } else {
+        return '0';
+      }
+    }
+
+    // Calculate max score.
+    function maxScore() {
+      if (frameTenScore) {
+        return frameTenScore;
+      } else if (frameNineScore) {
+        return frameNineScore + (30*1);
+      } else if (frameEightScore) {
+        return frameEightScore + (30*2);
+      } else if (frameSevenScore) {
+        return frameSevenScore + (30*3);
+      } else if (frameSixScore) {
+        return frameSixScore + (30*4);
+      } else if (frameFiveScore) {
+        return frameFiveScore + (30*5);
+      } else if (frameFourScore) {
+        return frameFourScore + (30*6);
+      } else if (frameThreeScore) {
+        return frameThreeScore + (30*7);
+      } else if (frameTwoScore) {
+        return frameTwoScore + (30*8);
+      } else if (frameOneScore) {
+        return frameOneScore + (30*9);
+      } else {
+        return '300';
+      }
+    }
+
+    // Print results to page
+    $("#frameOneScore").html(frameOneScore);
+    $("#frameTwoScore").html(frameTwoScore);
+    $("#frameThreeScore").html(frameThreeScore);
+    $("#frameFourScore").html(frameFourScore);
+    $("#frameFiveScore").html(frameFiveScore);
+    $("#frameSixScore").html(frameSixScore);
+    $("#frameSevenScore").html(frameSevenScore);
+    $("#frameEightScore").html(frameEightScore);
+    $("#frameNineScore").html(frameNineScore);
+    $("#frameTenScore").html(frameTenScore);
+    $("#currentScore").html(currentScore);
+    $("#maxScore").html(maxScore);
+
 });
-
-
-
-// Variable for each ball where A/B/C refers to the frame #, and 1/2 refers to the ball #.
-let A1 = 5;
-let A2 = 5;
-let B1 = 3;
-let B2 = 2;
-let C1 = '';
-let C2 = '';
-let D1 = '';
-let D2 = '';
-let E1 = '';
-let E2 = '';
-let F1 = '';
-let F2 = '';
-let G1 = '';
-let G2 = '';
-let H1 = '';
-let H2 = '';
-let I1 = '';
-let I2 = '';
-let J1 = '';
-let J2 = '';
-let K1 = '';
-let K2 = '';
-
-
-
-// Calculates results of frame one, taking into account strikes & spares.
-function calcFrameOne() {
-  if (A1===10 && B1===10 && C1) {
-    return A1+B1+C1;
-  } else if (A1===10 && B1 && B2) {
-    return A1+B1+B2;
-  } else if (A1+A2===10 && B1) {
-    return A1+A2+B1;
-  } else if (A1 && A2 && A1+A2<10) {
-    return A1+A2;
-  } else {
-    return '';
-  }
-}
-
-
-
-// Calculates results of frame two, taking into account strikes & spares.
-function calcFrameTwo() {
-  if (B1===10 && C1===10 && D1) {
-    return B1+C1+D1;
-  } else if (B1===10 && C1 && C2) {
-    return B1+C1+C2;
-  } else if (B1+B2===10 && C1) {
-    return B1+B2+C1;
-  } else if (B1 && B2 && B1+B2<10) {
-    return B1+B2;
-  } else {
-    return '';
-  }
-}
-
-
-
-// Calculates results of frame three, taking into account strikes & spares.
-function calcFrameThree() {
-  if (C1===10 && D1===10 && E1) {
-    return C1+D1+E1;
-  } else if (C1===10 && D1 && D2) {
-    return C1+D1+D2;
-  } else if (C1+C2===10 && D1) {
-    return C1+C2+D1;
-  } else if (C1 && C2 && C1+C2<10) {
-    return C1 + C2;
-  } else {
-    return '';
-  }
-}
-
-
-
-// Calculates results of frame four, taking into account strikes & spares.
-function calcFrameFour() {
-  if (D1===10 && E1===10 && F1) {
-    return D1+E1+F1;
-  } else if (D1===10 && E1 && E2) {
-    return D1+E1+E2;
-  } else if (D1+D2===10 && E1) {
-    return D1+D2+E1;
-  } else if (D1 && D2 && D1+D2) {
-    return D1+D2<10;
-  } else {
-    return '';
-  }
-}
-
-
-
-// Calculates results of frame five, taking into account strikes & spares.
-function calcFrameFive() {
-  if (E1===10 && F1===10 && G1) {
-    return E1+F1+G1;
-  } else if (E1===10 && F1 && F2) {
-    return E1+F1+F2;
-  } else if (E1+E2===10 && F1) {
-    return E1+E2+F1;
-  } else if (E1 && E2 && E1+E2) {
-    return E1+E2<10;
-  } else {
-    return '';
-  }
-}
-
-
-
-// Calculates results of frame six, taking into account strikes & spares.
-function calcFrameSix() {
-  if (F1===10 && G1===10 && H1) {
-    return F1+G1+H1;
-  } else if (F1===10 && G1 && G2) {
-    return F1+G1+G2;
-  } else if (F1+F2===10 && G1) {
-    return F1+F2+G1;
-  } else if (F1 && F2 && F1+F2<10) {
-    return F1+F2;
-  } else {
-    return '';
-  }
-}
-
-
-
-// Calculates results of frame seven, taking into account strikes & spares.
-function calcFrameSeven() {
-  if (G1===10 && H1===10 && I1) {
-    return G1+H1+I1;
-  } else if (G1===10 && H1 && H2) {
-    return G1+H1+H2;
-  } else if (G1+G2===10 && H1) {
-    return G1+G2+H1;
-  } else if (G1 && G2 && G1+G2<10) {
-    return G1+G2;
-  } else {
-    return '';
-  }
-}
-
-
-
-// Calculates results of frame eight, taking into account strikes & spares.
-function calcFrameEight() {
-  if (H1===10 && I1===10 && J1) {
-    return H1+I1+J1;
-  } else if (H1===10 && I1 && I2) {
-    return H1+I1+I2;
-  } else if (H1+H2===10 && I1) {
-    return H1+H2+I1;
-  } else if (H1 && H2 && H1+H2<10) {
-    return H1+H2;
-  } else {
-    return '';
-  }
-}
-
-
-
-// Calculates results of frame nine, taking into account strikes & spares.
-function calcFrameNine() {
-  if (I1===10 && J1===10 && K1) {
-    return I1+J1+K1;
-  } else if (I1===10 && J1 && J2) {
-    return I1+J1+J2;
-  } else if (I1+I2===10 && J1) {
-    return I1+I2+J1;
-  } else if (I1 && I2 && I1+I2<10) {
-    return I1+I2;
-  } else {
-    return '';
-  }
-}
-
-
-// Sets variable for each frame's score which equals the output of the calculate frame score function. 
-let frameOneScore = calcFrameOne()
-let frameTwoScore = calcFrameTwo()
-let frameThreeScore = calcFrameThree()
-let frameFourScore = calcFrameFour()
-let frameFiveScore = calcFrameFive()
-let frameSixScore = calcFrameSix()
-let frameSevenScore = calcFrameSeven()
-let frameEightScore = calcFrameEight()
-let frameNineScore = calcFrameNine()
-let totalScore = calcTotalScore()
-
-
-
-// Calculates total score of completed frames.
-function calcTotalScore() {
-  if (frameNineScore) {
-    return frameEightScore + frameSevenScore + frameSixScore + frameFiveScore + frameFourScore + frameThreeScore + frameTwoScore + frameOneScore;
-  } else if (frameEightScore) {
-    return frameSevenScore + frameSixScore + frameFiveScore + frameFourScore + frameThreeScore + frameTwoScore + frameOneScore;
-  } else if (frameSevenScore) {
-    return frameSixScore + frameFiveScore + frameFourScore + frameThreeScore + frameTwoScore + frameOneScore;
-  } else if (frameSixScore) {
-    return frameFiveScore + frameFourScore + frameThreeScore + frameTwoScore + frameOneScore;
-  } else if (frameFiveScore) {
-    return frameFourScore + frameThreeScore + frameTwoScore + frameOneScore;
-  } else if (frameFourScore) {
-    return frameThreeScore + frameTwoScore + frameOneScore;
-  } else if (frameThreeScore) {
-    return frameTwoScore + frameOneScore;
-  } else if (frameTwoScore) {
-    return frameOneScore;
-  }
-}
-
-
-// Prints total score + frame score once frame score score exists.
-function printFrameOne () {
-  if (frameOneScore) {
-      console.log (`Frame One: ` + frameOneScore);
-  } else {
-      console.log (`Frame One: Incomplete`);
-  } 
-}
-function printFrameTwo () {
-  if (frameTwoScore) {
-      console.log (`Frame Two: ` + (frameTwoScore + totalScore));
-  } else {
-      console.log (`Frame Two: Incomplete`);
-  } 
-}
-function printFrameThree () {
-  if (frameThreeScore) {
-      console.log (`Frame Three: ` + (frameThreeScore + totalScore));
-  } else {
-      console.log (`Frame Three: Incomplete`);
-  } 
-}
-function printFrameFour () {
-  if (frameFourScore) {
-      console.log (`Frame Four: ` + (frameFourScore + totalScore));
-  } else {
-      console.log (`Frame Four: Incomplete`);
-  } 
-}
-function printFrameFive () {
-  if (frameFiveScore) {
-      console.log (`Frame Five: ` + (frameFiveScore + totalScore));
-  } else {
-      console.log (`Frame Five: Incomplete`);
-  } 
-}
-function printFrameSix () {
-  if (frameSixScore) {
-      console.log (`Frame Six: ` + (frameSixScore + totalScore));
-  } else {
-      console.log (`Frame Six: Incomplete`);
-  } 
-}
-function printFrameSeven () {
-  if (frameSevenScore) {
-      console.log (`Frame Seven: ` + (frameSevenScore + totalScore));
-  } else {
-      console.log (`Frame Seven: Incomplete`);
-  } 
-}
-function printFrameEight () {
-  if (frameEightScore) {
-      console.log (`Frame Eight: ` + (frameEightScore + totalScore));
-  } else {
-      console.log (`Frame Eight: Incomplete`);
-  } 
-}
-function printFrameNine () {
-  if (frameNineScore) {
-      console.log (`Frame Nine: ` + (frameNineScore + totalScore));
-  } else {
-      console.log (`Frame Nine: Incomplete`);
-  } 
-}
-
-
-
-// Prints scores to console.
-printFrameOne()
-printFrameTwo()
-printFrameThree()
-printFrameFour()
-printFrameFive()
-printFrameSix()
-printFrameSeven()
-printFrameEight()
-printFrameNine()
